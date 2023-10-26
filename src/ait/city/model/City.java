1d92abcb19dfdab6e732ccd2d26f5c69cc93f16f
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-10-26 19:18:06 +0200

    code

---
+++ src/ait/city/model/City.java
@@ -0,0 +1,56 @@
+package ait.city.model;
+
+public class City implements Comparable<City> {
+    private String name;
+    private int population;
+
+    public City(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof City city)) return false;
+
+        if (getPopulation() != city.getPopulation()) return false;
+        return getName() != null ? getName().equals(city.getName()) : city.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + getPopulation();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", population=" + population +
+                '}';
+    }
+
+    @Override
+    public int compareTo(City o) {
+        return Integer.compare(population, o.population);
+    }
+}
